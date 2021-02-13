5165d3661f7baa5b7efabcd164759397575391f3
Tatang Sulaeman <am.tatang.sulaeman@gmail.com>
2021-02-13 18:15:22 +0700

    Finish landing page

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -38,6 +38,152 @@
         </div>
     </div>
 
+    <!-- Content -->
+    <div class="container custom-container-content">
+
+        <div class="row about-home">
+            <!-- about -->
+            <div class="col-span-2 mr-auto">
+                <h2>About</h2>
+            </div>
+            <div class="flex row justify-content-center about-home">
+                <div class="align-self-center col-sm figure-caption overflow-hidden">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+            </div>
+            <!-- about -->
+
+            <!-- Ekstrakurikuler -->
+            <div class="col-span-2 ml-auto">
+                <h2>Ekstrakurikuler</h2>
+            </div>
+            <div class="flex row justify-content-center about-home">
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="align-self-center col-sm figure-caption overflow-hidden">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+            <div class="flex row justify-content-center about-home">
+                <div class="align-self-center col-sm figure-caption overflow-hidden">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+            </div>
+
+            <div class="flex row justify-content-center about-home">
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="align-self-center col-sm figure-caption overflow-hidden">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+            <!-- Ekstrakurikuler -->
+
+            <!-- pencapapaian -->
+            <div class="col-span-2 mr-auto">
+                <h2>Pencapaian</h2>
+            </div>
+            <div class="flex row justify-content-center about-home">
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+                <div class="col-sm ">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+            </div>
+            <div class="flex row justify-content-center about-home">
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+                <div class="col-sm ">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+            </div>
+            <div class="flex row justify-content-center about-home">
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+                <div class="col-sm ">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+                <div class="col-sm">
+                    <img src="<?php echo base_url() ?>/assets/img/jumbotron_sekolah.jpg" class="figure-img img-fluid rounded" alt="about">
+                </div>
+                <div class="col-sm align-self-center">
+                    <h6>Lorem, ipsum dolor.</h6>
+                    <p>21/10/2020</p>
+                </div>
+            </div>
+            <!-- pencapaian -->
+        </div>
+    </div>
+    <!-- content -->
 
 </body>
 
